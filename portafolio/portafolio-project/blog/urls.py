00976c76398fc4cb289d0913b3aff47980578037
inecsoft <ivanpedrouk@gmail.com>
2020-04-16 18:04:16 +0100

    makemigration and migrate applied

--- portafolio/portafolio-project/blog/urls.py
+++ portafolio/portafolio-project/blog/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('blog/', include('blog.urls')),
-    #path('jobs/', include('blog.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #path('blog/', module.blog, name='portafolio-blog'),
+   ]
